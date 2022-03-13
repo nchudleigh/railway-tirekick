6b83410204091ed02dcd2cd9b3a5d45c8515fa4d
Neil Chudleigh <nchudleigh@gmail.com>
2022-03-13 10:32:13 -0400

    Add logging

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import os
 import asyncpg
 import datetime
 import asyncio
-from pypika import Query, Table, Field
+from pypika import Query, 
+import logging
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
 
 q = Query.from_("ping").select("id")
 
@@ -13,6 +17,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 async def main():
     # Establish a connection to an existing database named "test"
     # as a "postgres" user.
+    logging.info('Connecting to database')
     conn = await asyncpg.connect(DATABASE_URL)
     # Execute a statement to create a new table.
     # await conn.execute(q.get_sql())
@@ -27,14 +32,19 @@ async def main():
     # )
 
     # Select a row from the table.
+    logging.info('Fetching database row')
     row = await conn.fetchrow(q.get_sql())
     print(row)
+    logging.info(row)
 
     # *row* now contains
     # asyncpg.Record(id=1, name='Bob', dob=datetime.date(1984, 3, 1))
 
     # Close the connection.
+    logging.info('Closing database connection')
     await conn.close()
 
+
+logging.info('Starting up 🤘')
 
 asyncio.get_event_loop().run_until_complete(main())
